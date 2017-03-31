4358a714ee9abe31af71fbd5b699b2f741d3bf91
Devin Hurd <hurdaudio@gmail.com>
2017-03-31 08:33:43 -0600

    Admin communications center cleaned up

--- app/public/js/admin/admin.component.js
+++ app/public/js/admin/admin.component.js
@@ -31,6 +31,75 @@
       vm.serveUpFeedback = serveUpFeedback;
       vm.sortBy = '-created_at';
 
+      function cleanUpDates(messages) {
+        var time = "";
+        var day = "";
+        var month = "";
+        var year = "";
+
+        for (let i = 0; i < messages.length; i++) {
+          if (messages[i].responded) {
+            messages[i].response_status = "YES";
+          } else {
+            messages[i].response_status = "NO";
+          }
+          if (messages[i].created_at[11] === 0) {
+            time = messages[i].created_at.slice(12,16);
+          } else {
+            time = messages[i].created_at.slice(11,16);
+          }
+          if (messages[i].created_at[5] === 0) {
+            day = messages[i].created_at[6];
+          } else {
+            day = messages[i].created_at.slice(8, 10);
+          }
+          month = messages[i].created_at.slice(5, 7);
+          switch(month) {
+            case('01'):
+              month = "January";
+              break;
+            case('02'):
+              month = "February";
+              break;
+            case('03'):
+              month = "March";
+              break;
+            case('04'):
+              month = "April";
+              break;
+            case('05'):
+              month = "May";
+              break;
+            case('06'):
+              month = "June";
+              break;
+            case('07'):
+              month = "July";
+              break;
+            case('08'):
+              month = "August";
+              break;
+            case('09'):
+              month = "September";
+              break;
+            case('10'):
+              month = "October";
+              break;
+            case('11'):
+              month = "November";
+              break;
+            case('12'):
+              month = "December";
+              break;
+            default:
+              console.log('bad month data');
+          }
+          year = messages[i].created_at.slice(0,4);
+          messages[i].clean_date = day + "  " + month + "  " + year + "  at  " + time;
+        }
+        return (messages);
+      }
+
       function deleteFeedback(id) {
         console.log(id);
         if (getCookie("qwerty") === "whip it good") {
@@ -64,7 +133,8 @@
         if (getCookie("qwerty") === "whip it good") {
           $http.get('/visitor_feedbacks')
           .then(results=>{
-            vm.visitorFeedbacks = results.data;
+            vm.visitorFeedbacks = cleanUpDates(results.data);
+
           });
         }
       }
@@ -73,7 +143,8 @@
 
       function onInit() {
         console.log("Admin is lit.");
-
+        var adminAccess = document.getElementById('buttonAdmin');
+        adminAccess.setAttribute("style", "display: none;");
         var messageContent = document.getElementById('communicationsCenter');
         var commToggle = document.getElementById('toggleComm');
         messageContent.setAttribute("style", "display: none;");
