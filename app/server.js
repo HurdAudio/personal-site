a1662113352319ea546bf83b571663332b8c1dbe
Devin Hurd <hurdaudio@gmail.com>
2017-04-02 16:35:48 -0600

    shirley_jackson_awards routes

--- app/server.js
+++ app/server.js
@@ -91,6 +91,7 @@ const pulp_ark_new_pulp_awards = require('./routes/pulp_ark_new_pulp_awards.js')
 const rogers_writers_trust_fiction_prizes = require('./routes/rogers_writers_trust_fiction_prizes.js');
 const scotiabank_giller_prizes = require('./routes/scotiabank_giller_prizes.js');
 const shamus_awards = require('./routes/shamus_awards.js');
+const shirley_jackson_awards = require('./routes/shirley_jackson_awards.js');
 // const login = require('./routes/login.js');
 // const myPlaintextPassword = 'whip it';
 // const players = require('./routes/players.js');
@@ -193,6 +194,7 @@ app.use('/pulp_ark_new_pulp_awards', pulp_ark_new_pulp_awards);
 app.use('/rogers_writers_trust_fiction_prizes', rogers_writers_trust_fiction_prizes);
 app.use('/scotiabank_giller_prizes', scotiabank_giller_prizes);
 app.use('/shamus_awards', shamus_awards);
+app.use('/shirley_jackson_awards', shirley_jackson_awards);
 // app.use('/login', login);
 // app.use('/players', players);
 // app.use('/fantasyteams', fantasyteams);
